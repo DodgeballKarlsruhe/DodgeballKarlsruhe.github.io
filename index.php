442a2766529889de1c955f0effa84e61ebc6fe1d
SamiraPhotos <37023132+SamiraPhotos@users.noreply.github.com>
2018-10-04 20:17:16 +0200

    New Homepage

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dodgeball Karlsruhe</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/stylish-portfolio.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <a class="menu-toggle rounded" href="#">
+      <i class="fas fa-bars"></i>
+    </a>
+    <nav id="sidebar-wrapper">
+      <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+          <a class="js-scroll-trigger" href="#page-top">Dodgeball</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#news">Wann und Wo?</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#regeln">Regeln</a>
+        </li>
+        <li class="sidebar-nav-item">
+          <a class="js-scroll-trigger" href="#anmelden">Anmelden</a>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead d-flex">
+    </header>
+
+	<section style=height:10vw;>
+	</section>
+    <!-- About -->
+    <section id="news" style="background-color: #207089">
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-lg-10 mx-auto" style="padding: 5vw; color: white;">
+            <h1 class="text-secondary mb-0" style="text-align: center; text-transform: uppercase; letter-spacing: 2vw;">Wann und wo?</h2><hr><br>
+            <p>Sobald sich genug Mitspieler gefunden haben ist es wichtig, dass wir eine Halle finden. Ihr habt Connections? Lasst es uns wissen!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Regeln -->
+    <section class="content-section">
+      <div class="container" id="regeln">
+        <div class="content-section-heading text-center">
+          <h1 class="text-secondary mb-0" style="text-align: center; text-transform: uppercase; letter-spacing: 2vw;">Regeln</h1>
+		  <hr><br>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-4"><!--1-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Fairplay</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Dodgeball funktioniert nur mit Fairplay. Der Spirit-Gedanke steht über allem. Da auf Grund der Geschwindigkeit des Spiels sowie der Anzahl an Bällen nicht alles von den Schiedsrichtern gesehen werden kann, gilt: Fair spielen und das Schiedsrichterteam nach bestem Wissen und Gewissen unterstützen!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--2-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Spielaufbau</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Es wird 6 gegen 6 auf einem rechteckigen Spielfeld, welches in 2 Hälften geteilt wird, gespielt. Es gibt 6 Spielbälle.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--3-->
+              <a class="portfolio-item">
+                <span class="caption">
+                  <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                    <h2 style="text-align: center;">Spieleröffnung</h2>
+                    <p style="font-size: 1vw; padding: 2em; text-align:center;">Die 6 Spielbälle liegen zu Spielbeginn in der Mitte des Feldes. Die 6 Spieler des Teams stehen am hinteren Ende ihrer Spielfeldhälfte und berühren mit 1 Hand die Rückwand. Auf '3,2,1 Dodgeball' des Schiedsrichters laufen pro Mannschaft 2 Spieler los und holen die drei Bälle auf der rechten Seite. Nachdem die Bälle die hintere Wand berührt haben sind diese freigegeben.</p>
+                  </span>
+                </span>
+                <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+              </a>
+          </div>
+	      <div class="col-lg-4"><!--4-->
+	            <a class="portfolio-item">
+	              <span class="caption">
+	                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+	                  <h2 style="text-align: center;">Ziel des Spiels</h2>
+	                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Das Spiel endet, wenn alle Spieler eines Teams elimeniert wurden.</p>
+	                </span>
+	              </span>
+	              <img class="img-fluid" src="img/portfolio-2.jpg" alt="">
+	            </a>
+	      </div>
+          <div class="col-lg-4"><!--5-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Eliminieren</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Es gibt 3 Arten wie ein Spieler eliminiert werden kann:</br>
+A) Durch das Abschießen eines Gegenspielers. Der getroffene Spieler muss das Spielfeld verlassen.</br>
+B) Durch Fangen eines geworfenen Balls. Der Spieler der den Ball geworfen hat muss das Spielfeld verlassen.</br>
+C) Beim Übertritt oder Berührung einer Spielfeldbegrenzung.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--6-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Fangen</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Zusätzlich zu der Tatsache, dass durch Fangen eines Balles der Werfer „raus muss“, kommt zu der Mannschaft des Fängers ein bereits eliminierter Spieler wieder zurück aufs Feld.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--7-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Abwerfen</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Ein Spieler ist abgeschossen, wenn er von einem geworfenen Ball der gegnerischen Mannschaft getroffen wird und der Ball danach den Boden, die Wand oder jeglichen anderen Körper/Gegenstand berührt.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--8-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Blocken</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Sollte man einen Ball zur Verteidigung in Händen halten und einen herankommenden Ball damit abblocken, so ist man nicht abgeschossen. Sollte man jedoch den Blockball dabei aus den Händen verlieren, so gilt das als Abschuss.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--9-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Fischen</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Das „Fischen“ ist sowohl über die Seitenlinien hinaus, als auch in das gegnerischen Spielfeld hinüber, erlaubt.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--10-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Zugzwang</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Damit das Spiel kontinuierlich läuft und Zeitspiel unterbunden wird darf ein Ball nur 10 Sekunden in der Hand gehalten werden, bevor der Ball geworfen werden muss. Die Zeit läuft weiter, auch wenn der Ball einem Mitspieler übergeben wird.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--11-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Die letzten 2 Spieler</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Sollte bei beiden Mannschaft nur noch 1 Spieler übrig sein, so starten separat 10 Sekunden. Wird in dieser Zeit kein Spieler eliminiert, so kommt es zum Sudden Death.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--12-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Sudden Death</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Beide übriggebliebenen Spieler bekommen jeweils ein Ball und müssen die Rückwand ihres Feldes berühren. Auf der Mittellinie liegen rechts und links jeweils zwei Bälle. Die Mittellinie hat keine Bedeutung mehr. Wer als erstes abgeworfen wird ist dabei raus. </p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--13-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Kopfhöhe</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Wird eine Person am Kopf getroffen, so müssen beide Spieler raus. Sowohl der Werfer, als auch der Abgeworfene.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--14-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Spielverlauf</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Bei einem Turnier gilt Jeder-gegen-Jeden. Man spielt drei Mal hintereinander gegen die selbe Mannschaft mit Seitenwechsel nach Spielende. Die Spieler werden in dieser Zeit nicht getauscht. Jedes Spiel zählt einzeln in die Wertung. </br> Jede Mannschaft muss 2 Mädchen/Frauen auf dem Feld haben. </p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-3.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-4"><!--15-->
+            <a class="portfolio-item">
+              <span class="caption">
+                <span style="padding:1vw; color: white; font-family: monospace; padding-top: 3vw;">
+                  <h2 style="text-align: center;">Regeln</h2>
+                  <p style="font-size: 1vw; padding: 2em; text-align:center;">Die Regeln wurden angelehnt an http://dodgeball.at/regeln-vereinfacht/ zusammengefasst. Veränderungen ergaben sich, da die amerikanischen Regeln genommen wurden.</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+      </div>
+    </section>
+	
+    <!-- Callout -->
+    <section class="callout" style="background-color: #207089">
+      <div class="container text-center" id="anmelden">
+        <h1 class="text-secondary mb-0" style="text-align: center; text-transform: uppercase; letter-spacing: 2vw;">Anmelden</h1>
+		<hr><br>
+		<form method="post" action="mailhandler.php" name="form">
+			<div style="text-align: center; font-weight: bold; width=100%; color: white;">
+				<label>Vorname</label>
+				<input type="text" name="name" placeholder="Tim, Tom, Tan" class="form-control form-control-sm" size="30" maxlength="50" style="width: 20%; margin-left: 40%;"><br>
+				<label>E-Mail</label>
+				<input type="email" name="email" class="form-control form-control-sm" placeholder="name@example.com" size="30" maxlength="50" style="width: 20%; margin-left: 40%;"><br>
+				<label>Nummer</label>
+				<input type="text" name="phone" placeholder="0123/456789" class="form-control form-control-sm" size="30" maxlength="50" style="width: 20%; margin-left: 40%;"><br>
+				<label>Geschlecht</label>
+				    <select name="sex" class="form-control" id="exampleFormControlSelect1" class="form-control form-control-sm" style="width: 20%; margin-left: 40%;"><br>			
+				      <option>weiblich</option>
+				      <option>männlich</option>
+				      <option>keine Angabe</option>
+				    </select><br>
+				<label>Passender Termin</label><br>
+				<div class="form-check form-check-inline">
+				  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+				  <label class="form-check-label" for="inlineCheckbox1">Montag</label>
+				</div>
+				<div class="form-check form-check-inline">
+				  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+				  <label class="form-check-label" for="inlineCheckbox1">Dienstag</label>
+				</div>
+				<div class="form-check form-check-inline">
+				  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+				  <label class="form-check-label" for="inlineCheckbox1">Mittwoch</label>
+				</div>
+				<div class="form-check form-check-inline">
+				  <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+				  <label class="form-check-label" for="inlineCheckbox1">Donnerstag</label>
+				</div>
+				<div class="form-check form-check-inline">
+				  <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+				  <label class="form-check-label" for="inlineCheckbox2">Freitag</label>
+				</div><br><br>
+				<input type="Submit" class="btn btn-outline-light" name="absenden" value="Senden">	
+			</div>
+		</form>
+      </div>
+    </section>
+
+    <!-- Map -->
+   <section id="contact" class="map">
+   <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d37477.42831417878!2d8.364688177084753!3d49.009337188962206!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sde!2sde!4v1538669917454" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      <br/>
+      <small>
+      </small>
+    </section> 
+
+
+    <!-- Footer -->
+<!--
+    <footer class="footer text-center">
+      <div class="container">
+        <ul class="list-inline mb-5">
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-facebook"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white mr-3" href="#">
+              <i class="icon-social-twitter"></i>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="social-link rounded-circle text-white" href="#">
+              <i class="icon-social-github"></i>
+            </a>
+          </li>
+        </ul>
+        <p class="text-muted small mb-0">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+-->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded js-scroll-trigger" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/stylish-portfolio.min.js"></script>
+
+  </body>
+
+</html>
